dc6de879daafb0b79ef219e8180048a5b0672b4b
Abry Rath <abry.rath@union.co>
2018-12-12 08:14:30 -0500

    Drag/drop

--- resources/js/modules/FileUpload.js
+++ resources/js/modules/FileUpload.js
@@ -1,22 +1,33 @@
+const removeDragData = e => {
+    if (e.dataTransfer.items) {
+        e.dataTransfer.items.clear();
+    } else {
+        e.dataTransfer.clearData();
+    }
+};
+
 class FileUpload {
     constructor(node) {
         this.element = node;
         this.uploadUrl = '/admin/import/upload';
-        console.log('FileUpload');
-        this.input = document.querySelector('[data-import-file-upload] > input[type="file"]');
+        this.label = 'Choose a file';
+        this.files = [];
+
+        // Drag/drop
+        this.element.addEventListener('drop', this.dropHandler.bind(this));
+        this.element.addEventListener('dragover', this.dragOverHandler.bind(this));
+
+        // Input type="file"
+        this.input = document.querySelector('input[type="file"].FileUpload-input');
         if (this.input) {
-            this.input.addEventListener('change', this.upload.bind(this));
+            this.input.addEventListener('change', this.fileSelectHandler.bind(this));
         }
     }
 
-    upload(e) {
-        this.input.disabed = true;
-
+    upload() {
         const formData = new FormData();
-
-        // some data check
-        const files = e.target.files;
-        Array.prototype.forEach.call(files, f => {
+        
+        Array.prototype.forEach.call(this.files, f => {
             formData.append('files[]', f, f.name);
         });
 
@@ -33,6 +44,60 @@ class FileUpload {
                 const container = document.querySelector('[data-import-file-upload-result]');
                 container.innerHTML = data;
             });
+    }
+
+    fileSelectHandler(e) {
+        this.input.disabed = true;
+        this.files = e.target.files;
+        this.updateLabel();
+
+        this.upload();
+    }
+
+    dropHandler(e) {
+        console.log(e);
+        e.preventDefault();
+
+        if (e.dataTransfer.items) {
+            Array.prototype.forEach.call(e.dataTransfer.items, item => {
+                if (item.kind === 'file') {
+                    const file = item.getAsFile();
+                    this.files.push(file);
+                }
+            });
+        } else {
+            Array.prototype.forEach.call(e.dataTransfer.files, file => {
+                this.files.push(file);
+            })
+        }
+
+        this.updateLabel();
+        removeDragData(e);
+        this.upload();
+
+        return;
+    }
+
+    dragOverHandler(e) {
+        e.preventDefault();
+
+        return;
+    }
+
+    updateLabel() {
+        this.label = '';
+        if (this.files && this.files.length) {
+            console.log(this.files);
+            Array.prototype.forEach.call(this.files, file => {
+                if (this.label.length) {
+                    this.label += ', ';
+                }
+                this.label += file.name;
+            })
+        } else {
+            this.label = 'Choose a file';
+        }
+        document.querySelector('.FileUpload-input + label>span').innerText = this.label;
     }
 }
 
